db2e2f8952fe5bd36ae6664c14518bc368552a8c
Matthew0898 <Matthew0898@Matthew0898.com>
2019-11-28 15:04:19 -0600

    Minor Tweaks

--- src/it226/Run.java
+++ src/it226/Run.java
@@ -1,10 +1,10 @@
 package it226;
 
+
+
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
